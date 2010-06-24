05997ec19e40ec9e7189cfaf8def7d4b78b8efb3
Ariel Núñez <ingenieroariel@gmail.com>
2010-06-23 23:11:22 -0500

    Removed pluggables dir, we can use a requirements file instead

--- manage.py
+++ manage.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import sys
-from os.path import join
 from django.core.management import execute_manager
 try:
     import settings # Assumed to be in the same directory.
@@ -8,8 +7,6 @@ except ImportError:
     import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
-
-sys.path.insert(0, join(settings.PROJECT_DIR, "pluggables"))
 
 if __name__ == "__main__":
     execute_manager(settings)
