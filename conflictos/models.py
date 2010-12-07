7034cbfc6670a6d03c92551cab20c335faaed030
panchicore <panchicore@panchicore-kub.(none)>
2010-12-07 12:01:46 -0500

    new model for conflictos

--- conflictos/models.py
+++ conflictos/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from fuentes.models import FuenteDato
 
 # Create your models here.
+TIPO_CONFICTOS = (
+    ("cultural","Cultural"),
+    ("politico-violencia","Político / Violencia"),
+    ("politico-pgarmados","Político / Presencia Grupos armados"),
+    ("politico-electoral","Político / Político electoral"),
+    ("economico","Económico"),
+    ("recursos-nat","Recursos Naturales"),
+    ("terr-uso","Territorial / Uso"),
+    ("terr-delimitacion","Territorial / Delimitación"),
+    ("otros","Otros")
+)
+
+class Conflicto(models.Model):
+    content_type = models.ForeignKey(ContentType, blank=True, null=True, related_name="content_type_conflicto")
+    object_id = models.PositiveIntegerField(blank=True, null=True)
+
+    content_object = generic.GenericForeignKey()
+
+    categoria = models.CharField(max_length=50, choices=( ("intraetnico","Intraétnico"),("interetnico","Interétnico") ) )
+    tipo = models.CharField(max_length=50, choices=TIPO_CONFICTOS)
+    """ descripcion """
+    ubicacion = models.TextField(null=True, blank=True)
+    actores = models.TextField(null=True, blank=True)
+    hechos = models.TextField(null=True, blank=True)
+    estado = models.CharField(null=True, blank=True, choices=( ("abierto","Abierto"),("cerrado","Cerrado") ) )
+    fecha = models.DateField(null=True, blank=True)
+    fuente = models.ForeignKey(FuenteDato, null=True, blank=True, related_name="conflictos")
+
