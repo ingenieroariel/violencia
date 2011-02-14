014ab2ddda8d6ff789be8024af5fdcd6652fc3cd
panchicore <panchicore@panchicore-XPS.(none)>
2011-02-14 11:51:30 -0500

    se agrega campo de año en fuentes

--- fuentes/admin.py
+++ fuentes/admin.py
@@ -4,6 +4,20 @@ from fuentes.models import AutorDato, FuenteDato
 class FuenteDatoAdmin(admin.ModelAdmin):
         list_display= ('nombre_documento', 'autor', 'fecha', 'fecha_ingreso',
                        'archivo', 'usuario', 'notas')
+        fieldsets = (
+                (None, {
+                      'fields':
+                         (
+                         ('nombre_documento','autor'),
+                         ('fecha','ano'),
+                         'frecuencia_actualizacion',
+                         ('fecha_ingreso','usuario'),
+                         
+                         'archivo',
+                         'notas',
+                         ),
+                  }),
+            )
 
 admin.site.register(AutorDato)
 admin.site.register(FuenteDato, FuenteDatoAdmin)
